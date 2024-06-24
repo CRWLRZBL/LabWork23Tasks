62a847de2c3476c6ce66170c5a14c7313a8e3388
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:45:50 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using LabWorkLibrary;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Сумма = {Maths.GetSum(40, 24)}");
+            Console.WriteLine($"Разность = {Maths.GetDifference(40, 24)}");
+            Console.WriteLine($"Произведение = {Maths.GetMultiplication(40, 24)}");
+            Console.WriteLine($"Частное = {Maths.GetDivision(40, 24)}");
+            Console.WriteLine($"Площадь = {Maths.GetRectangleArea(40, 24)}");
+            Console.WriteLine($"Константа = {Maths.BINARY_FACTOR}");
+        }
+    }
+}
